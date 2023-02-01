d79633bf38005d0af479229d7881dca215482f09
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-02-01 09:30:05 +0100

    hoofdstuk 1 en 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kennismakenprogrameeromgeving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello wolrd!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rekenen/Program.cs
@@ -0,0 +1,39 @@
+﻿// opdracht 1
+Console.WriteLine(6 + 3);
+Console.WriteLine(209 + 80);
+// opdracht 2
+Console.WriteLine(7+8+345+9+3+23+10);
+Console.WriteLine(19253-1243);
+//opdracht 3
+Console.WriteLine(17 - 19);
+//opdracht 4
+Console.WriteLine(17.15 - 19.7234);
+//opdracht 5
+Console.WriteLine(1962 - 62 - 19 - 0.5);
+Console.WriteLine(150.44 * 12.5);
+//opdracht 6
+// Console.WriteLine(132597 * 575688);
+Console.ReadKey();
+Console.WriteLine(1 * 7);
+Console.WriteLine(2 * 7);
+Console.WriteLine(3 * 7);
+Console.WriteLine(4 * 7);
+Console.WriteLine(5 * 7);
+Console.WriteLine(6 * 7);
+Console.WriteLine(7 * 7);
+Console.WriteLine(8 * 7);
+Console.WriteLine(9 * 7);
+Console.WriteLine(10 * 7);
+Console.ReadKey();
+Console.WriteLine(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9);
+//opdracht 7
+Console.WriteLine(-5 * 3 * -9);
+Console.WriteLine(24 / 4);
+//opdracht 8
+Console.WriteLine(23 / 5);
+Console.WriteLine(76 % 13);
+//opdracht 9
+Console.WriteLine(75 % 29);
+Console.WriteLine(Math.Pow(8, 2));
+//opdracht 10
+Console.WriteLine(5 * 5 * 5 - Math.Pow(5, 4));
